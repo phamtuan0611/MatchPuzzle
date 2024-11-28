1929ea8c447de39f4462facd89c93296ac7f0719
phamtuan0611 <phamtuan06112003@gmail.com>
2024-11-28 22:55:31 +0700

    Update UI

---
+++ Assets/_Game/Scripts/RoundManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public float roundTime = 60f;
+    private UIManager uiMan;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        uiMan = FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roundTime > 0)
+        {
+            roundTime -= Time.deltaTime;
+
+            if (roundTime <= 0)
+            {
+                roundTime = 0;
+            }
+        }
+
+        uiMan.timeText.text = roundTime.ToString("0.0") + "s";
+    }
+}
